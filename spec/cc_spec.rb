7c278c801e448494badde7199606e42e81c12ef0
donnymays <dtmays2@gmail.com>
2020-09-24 10:33:59 -0700

    passing tests for all coin methods

--- spec/cc_spec.rb
+++ spec/cc_spec.rb
@@ -8,10 +8,24 @@ describe("CC#quarters") do
   end
 end
   
-  describe("CC#dimes") do
+describe("CC#dimes") do
   it("correctly returns the amount of dime when given a total amount of cents") do
     coin = CC.new()
-    expect(coin.dimes(63)).to(eq(2))
+    expect(coin.dimes(63)).to(eq(6))
+  end
+end
+
+describe("CC#nickels") do
+  it("correctly returns the amount of dime when given a total amount of cents") do
+    coin = CC.new()
+    expect(coin.nickels(13)).to(eq(2))
+  end
+end
+
+describe("CC#pennies") do
+  it("correctly returns the amount of dime when given a total amount of cents") do
+    coin = CC.new()
+    expect(coin.pennies(63)).to(eq(63))
   end
 end
 
