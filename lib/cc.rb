c9f08c89c6d9a7739468e8fcff0677317fa54ff9
donnymays <dtmays2@gmail.com>
2020-09-24 10:32:29 -0700

    successful fail of dimes method

--- lib/cc.rb
+++ lib/cc.rb
@@ -5,20 +5,38 @@ class CC
     @nickels = 0
     @pennies = 0
   end
+  
   def quarters(input)
     until input < 25 do
-      @quarters = @quarters + 1 
-      input = input - 25
+      @quarters += 1 
+      input -= 25
     end
     return @quarters
   end
   
   def dimes(input)
     until input < 10 do
-      input -= 10
       @dimes += 1
+      input -= 10  
     end
+    return @dimes
   end   
+  
+  def nickels(input)
+    until input < 5 do
+      @nickels += 1
+      input -= 5  
+    end
+    return @nickels
+  end  
+  
+  def pennies(input)
+    until input < 1 do
+      @pennies += 1
+      input -= 1  
+    end
+    return @pennies
+  end    
   
   def coins
     
--- spec/cc_spec.rb
+++ spec/cc_spec.rb
@@ -2,10 +2,18 @@ require('rspec')
 require('cc')
 
 describe("CC#quarters") do
-  it("correctly setup the initialize method") do
+  it("correctly returns the amount of quarters when given a total amount of cents") do
     coin = CC.new()
     expect(coin.quarters(63)).to(eq(2))
   end
-
 end
+  
+  describe("CC#dimes") do
+  it("correctly returns the amount of dime when given a total amount of cents") do
+    coin = CC.new()
+    expect(coin.dimes(63)).to(eq(2))
+  end
+end
+
+
 
